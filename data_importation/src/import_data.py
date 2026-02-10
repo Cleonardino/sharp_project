e9ef5c9cb96345e0a58f95de535ac8aa4883664d
Clement Desberg <clement@desberg.fr>
2026-02-10 01:51:56 +0100

    feat: import_data successfull connect to picsellia

--- data_importation/src/import_data.py
+++ data_importation/src/import_data.py
@@ -1,2 +1,13 @@
-from picsellia import Client,
-client = Client(api_token="$TOKEN$", organization_name="$ORGANIZATION_NAME$", host="https://trial.picsellia.com")
+from picsellia import Client
+from dotenv import load_dotenv
+import os
+
+load_dotenv("config/.env")
+
+PICSELLIA_TOKEN = os.getenv("PICSELLIA_TOKEN")
+ORGANIZATION = "clement4"
+
+client = Client(
+    api_token=PICSELLIA_TOKEN,
+    organization_name=ORGANIZATION
+    )
