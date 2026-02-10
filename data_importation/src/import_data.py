6bd8bce04ddb7e383985e8511cfa562f695893d5
Clement Desberg <clement@desberg.fr>
2026-02-10 02:17:03 +0100

    fix: changed import_data to a module

--- data_importation/src/import_data.py
+++ data_importation/src/import_data.py
@@ -1,7 +1,7 @@
 from picsellia import Client
 from dotenv import load_dotenv
 import os
-import config as cf
+import data_importation.src.config as cf
 
 def download_dataset():
     load_dotenv("config/.env")
