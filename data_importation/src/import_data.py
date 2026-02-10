7d333b479cf481b50820c1a5cfdecda492dda3b3
Clement Desberg <clement@desberg.fr>
2026-02-10 02:00:02 +0100

    feat: updated import_data

--- data_importation/src/import_data.py
+++ data_importation/src/import_data.py
@@ -5,12 +5,23 @@ import os
 load_dotenv("config/.env")
 
 PICSELLIA_TOKEN = os.getenv("PICSELLIA_TOKEN")
-ORGANIZATION = os.getenv("ORGANIZATION")
-DATASET = os.getenv("DATASET")
+PICSELLIA_ORGANIZATION = os.getenv("PICSELLIA_ORGANIZATION")
+PICSELLIA_DATASET = os.getenv("PICSELLIA_DATASET")
+PICSELLIA_VERSION = os.getenv("PICSELLIA_VERSION")
 
 client = Client(
     api_token=PICSELLIA_TOKEN,
-    organization_name=ORGANIZATION
+    organization_name=PICSELLIA_ORGANIZATION
     )
 
-dataset = client.get_dataset(name=DATASET)
+dataset = client.get_dataset(name=PICSELLIA_DATASET)
+
+dataset_version = dataset.get_version(PICSELLIA_VERSION)
+
+output_dir = "./dataset"
+dataset_version.download(
+    target_path=output_dir,
+    with_annotations=True
+)
+
+print(f"import_data -> Dataset downloaded to {output_dir}")
